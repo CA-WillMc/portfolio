2cefd7cc63f6c1c8f137d634325ac115b483eb39
CA-WillMc <122403149+CA-WillMc@users.noreply.github.com>
2024-01-19 00:04:18 -0500

    style, animations

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var body = document.body;
+
+  window.addEventListener("scroll", function () {
+    var scrollPosition = window.scrollY;
+    var scrollPercentage =
+      (scrollPosition / (document.body.scrollHeight - window.innerHeight)) *
+      100;
+
+    // Calculate the opacity based on scroll position
+    var opacity = 1 - scrollPercentage / 100;
+
+    // Apply fading effect
+    if (scrollPercentage < 50) {
+      opacity = 1 - scrollPercentage / 100;
+    } else {
+      opacity = scrollPercentage / 100;
+    }
+    body.style.backgroundColor = "rgba(173, 216, 230," + opacity + ")";
+  });
+});
